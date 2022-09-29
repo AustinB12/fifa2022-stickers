f235ba5767ffce65996986d3385cff3c78156001
Austin Baird <austinzbaird@gmail.com>
2022-09-29 14:36:32 -0400

    removed api stuff

--- pages/api/updateStickers.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-
-export default async function handler(req, res) {
-  await fs.writeFileSync('stickers.json', JSON.stringify(req.body))
-  return res.status(200).json({})
-}
